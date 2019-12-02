6dea0d81af856e2073c1cbea3d9580b4a28d547a
angela balaguer <balaguer.valles@gmail.com>
2019-12-01 19:34:33 -0500

    añadiendo receptor de notificaciones

--- app/src/main/java/com/example/taller4/Servicio.java
+++ app/src/main/java/com/example/taller4/Servicio.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class Servicio extends Activity {
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         setContentView(R.layout.activity_servicio);
         Button arrancar = (Button) findViewById(R.id.boton_arrancar);
         arrancar.setOnClickListener(new View.OnClickListener() {
@@ -19,6 +24,7 @@ public class Servicio extends Activity {
                         ServicioMusica.class));
             }
         });
+
         Button detener = (Button) findViewById(R.id.boton_detener);
         detener.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
